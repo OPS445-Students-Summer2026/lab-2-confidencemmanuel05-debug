2240340cd18e4cf5fbdc9fe58d41df93b3329227
Confidence Emmanuel <caemmanuel@myseneca.ca>
2026-05-20 14:28:48 -0400

    completed lab 2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: Confidence Andrea Emmanuel
+# Author ID: caemmanuel@myseneca.ca
+# Date Created: 2026/05/20
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
